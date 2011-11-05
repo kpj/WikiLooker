c28ef1107fb2d9422720bf608ea681134b4ac609
kpj <kpjkpjkpjkpjkpjkpj@googlemail.com>
2011-11-05 12:11:10 +0100

    Minor bugfixes

--- wikiLooker3.py
+++ wikiLooker3.py
@@ -16,24 +16,24 @@ tries = 1
 triedSnip = []
 def validateUrl(urlSnip):
 	global tries, triedSnip
-	print('			 Try %u: "%s"...'%(tries, urlSnip), end="")
+	print('    Try %u: "%s"...'%(tries, urlSnip), end="")
 	for arg in ['wiktionary', 'wikimedia', '.ogg', 'simple.wikipedia', '/File:', '/Wikipedia:']: # Bad keywords
 		if arg in urlSnip:
 			print("	[bad keyword (%s)]"%(arg), end="\n")
 			tries += 1
-			return True
+			return False
 	for entry in triedSnip:
 		if entry == urlSnip:
 			print("	[repetition detected]", end="\n")
 			tries += 1
-			return True
+			return False
 	if '/wiki' in urlSnip:
 		triedSnip.append(urlSnip)
 		print("", end="\n")
-		return False
+		return True
 	tries += 1
 	print("	[no /wiki found]", end="\n")
-	return True
+	return False
 
 def deleteHTMLTags(code, tags):
 	for tag in tags:
@@ -61,8 +61,6 @@ def doStuff(url):
 		#print p
 		hrefsInB = re.compile(r'\(.*?<a href="(.*?)".*?\)').findall(p) # Get hrefs surrounded by brackets
 		hrefs = re.compile(r'<a href="(.*?)"').findall(p) # Get link value
-		#print hrefs
-		#print hrefsInB
 		# Delete Links in brackets out of normal links-list
 		for link in hrefs:
 			for linkB in hrefsInB:
@@ -80,15 +78,15 @@ def doStuff(url):
 			print(">>> No links in brackets found...")
 			if 'may refer to:' in code:
 				print(">>> We are on a selection page...")
-				liH = []
 				inLi = re.compile(r'<li>(.+?)</li>').findall(code)
 				for li in inLi:
 					checker(re.compile(r'<a href="(.*?)"').findall(li))
 			sys.exit()
 
 def checker(links):
+	global tries
 	c = 0
-	while validateUrl(links[c]):
+	while not validateUrl(links[c]):
 		c += 1
 		if c == len(links):
 			print('>>> No validated url found, aborting...')
